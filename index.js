ad35097cc77cf6a34167a3a106a894f546e86d5a
cthuluhoop123 <dowzhong@gmail.com>
2020-10-12 15:22:03 +1100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const utils = require('./utils.js');
 
 const schedule = require('node-schedule');
 
-client.once('ready', () => {
+client.once('ready', async () => {
     schedule.scheduleJob('0 0 * * *', async () => {
         const events = await utils.getAllEvents({
             expired: false,
@@ -19,4 +19,29 @@ client.once('ready', () => {
             utils.expireEvent(guild, event.id).catch(err => console.error(`Could not expire event ${event.id}`, err));
         });
     });
-})
+
+    schedule.scheduleJob('0 13 * * *', async () => {
+        const tomorrowStart = new Date();
+        tomorrowStart.setHours(0, 0, 0, 0);
+        tomorrowStart.setDate(tomorrowStart.getDate() + 1);
+
+        const tomorrowEnd = new Date();
+        tomorrowEnd.setHours(0, 0, 0, 0);
+        tomorrowEnd.setDate(tomorrowEnd.getDate() + 2);
+
+        const events = await utils.getAllEvents({
+            expired: false,
+            date: {
+                [database.Sequelize.Op.between]: [tomorrowStart, tomorrowEnd]
+            }
+        });
+
+        events.forEach(async event => {
+            const guild = await client.guilds.fetch(event.guildId).catch(err => null);
+            if (!guild) return;
+            const { eventTalk } = utils.getEventsChannels(guild);
+            eventTalk.send(`Hey <@&${event.roleId}>, just a reminder that your event is coming up tomorrow!`)
+                .catch(err => { });
+        });
+    });
+});
